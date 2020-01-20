8153dffd6b2f82d25de7b2563913c2350778da22
Evgeny <jenyok89@mail.ru>
2020-01-20 12:04:41 +0300

    edit js

--- ICL/Schemas/ActivitySectionV2/ActivitySectionV2.js
+++ ICL/Schemas/ActivitySectionV2/ActivitySectionV2.js
@@ -3,6 +3,7 @@ define("ActivitySectionV2", [], function() {
 		entitySchemaName: "Activity",
 		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
 		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
+		methods: {
+}
 	};
 });
