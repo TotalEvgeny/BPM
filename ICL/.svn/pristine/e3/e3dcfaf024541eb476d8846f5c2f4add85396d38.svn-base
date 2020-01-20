ede60525d92488f4d67589193f69f334fb67c612
Alexander <aaivanov86@gmail.com>
2020-01-20 11:25:22 +0300

    deleting old

--- ICL/.svn/pristine/e3/e3dcfaf024541eb476d8846f5c2f4add85396d38.svn-base
+++
@@ -1,8 +0,0 @@
-define("ActivitySectionV2", [], function() {
-	return {
-		entitySchemaName: "Activity",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
--- ICL/Schemas/ICLSchema2Detail/ICLSchema2Detail.js
+++
@@ -1,8 +0,0 @@
-define("ICLSchema2Detail", [], function() {
-	return {
-		entitySchemaName: "ICLCaseReminder",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
